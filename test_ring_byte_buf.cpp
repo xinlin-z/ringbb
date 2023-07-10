cbc40f943034a967e924455e6f8c40e16394e4b2
xinlin-z <xinlin.zhang@hotmail.com>
2023-07-10 14:06:20 +0800

    update test

--- test_ring_byte_buf.cpp
+++ test_ring_byte_buf.cpp
@@ -122,51 +122,110 @@ void test_push_back_3(){
 }
 
 
-TEST(ring_byte_buf, 0002){
-    ring_byte_buf<char> rb{8};
-    EXPECT_EQ(rb.empty(), true);
-    EXPECT_EQ(rb.capacity(), 8);
-    EXPECT_EQ(rb.size(), 0);
-    string a {"12345678"};
-    rb.push_back(a);
-    rb.push_back(a);
-    EXPECT_EQ(rb.capacity(), 16);
-    EXPECT_EQ(rb.size(), 16);
-    EXPECT_EQ(rb.empty(), false);
-    byte b[9] {};
-    rb.pop_front(b, 9);
-    EXPECT_EQ(rb.capacity(), 16);
-    EXPECT_EQ(rb.size(), 7);
-    EXPECT_EQ(memcmp(b,"123456781",9), 0);
+void test_pop_front(){
+    ring_byte_buf rb;
+    rb.push_back("12345", 5);
+    HOPE_EQ(rb.size(), 5);
+    HOPE_EQ(rb.capacity(), 8);
+    int n;
+    char x;
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(x, '1');
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(x, '2');
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(x, '3');
+    rb.shrink();
+    HOPE_EQ(rb.size(), 2);
+    HOPE_EQ(rb.capacity(), 2);
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(x, '4');
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(x, '5');
+    n = rb.pop_front(&x, 1);
+    HOPE_EQ(n, 0);
+    HOPE_EQ(x, '5');
+    HOPE_EQ(rb.size(), 0);
+    HOPE_EQ(rb.capacity(), 2);
+    rb.shrink();
+    HOPE_EQ(rb.capacity(), 0);
 }
 
 
-TEST(ring_byte_buf, 0003){
-    ring_byte_buf<uint8_t> u8rb;
-    for(int i=0; i<1024*1024+1; ++i)
-        u8rb.push_back("9");
-    EXPECT_EQ(u8rb.capacity(), 1024*1024*2);
-    EXPECT_EQ(u8rb.size(), 1024*1024+1);
+void test_pop_front_2(){
+    ring_byte_buf rb;
+    rb.push_back("abcdefg", 7);
+    int n;
+    char c2[2] {};
+    n = rb.pop_front(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"ab",2), 0);
+    n = rb.pop_front(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"cd",2), 0);
+    n = rb.pop_front(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"ef",2), 0);
+    n = rb.pop_front(c2, 2);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(memcmp(c2,"gf",2), 0);
+}
+
+
+void test_push_front_pop_back(){
+    ring_byte_buf rb;
+    rb.push_front("1234567", 7);
+    int n;
+    char c2[2] {};
+    n = rb.pop_back(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"67",2), 0);
+    n = rb.pop_back(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"45",2), 0);
+    n = rb.pop_back(c2, 2);
+    HOPE_EQ(n, 2);
+    HOPE_EQ(memcmp(c2,"23",2), 0);
+    n = rb.pop_back(c2, 2);
+    HOPE_EQ(n, 1);
+    HOPE_EQ(memcmp(c2,"13",2), 0);
+}
+
+
+void test_stress(){
+    ring_byte_buf rb;
     uint8_t b[4] {};
+
+    for(int i=0; i<1024*1024+1; ++i)
+        rb.push_back('9');
+    HOPE_EQ(rb.capacity(), 1024*1024*2);
+    HOPE_EQ(rb.size(), 1024*1024+1);
     for(int i=0; i<1024*1024+1; ++i){
-        u8rb.pop_front(b, 1);
-        EXPECT_EQ(b[0], '9');
+        rb.pop_front(b, 1);
+        HOPE_EQ(b[0], '9');
     }
-    EXPECT_EQ(u8rb.capacity(), 1024*1024*2);
-    EXPECT_EQ(u8rb.size(), 0);
+    HOPE_EQ(rb.capacity(), 1024*1024*2);
+    HOPE_EQ(rb.size(), 0);
+
     string a {"90"};
     for(int i=0; i<1024*1024+1; ++i)
-        u8rb.push_back(a);
-    EXPECT_EQ(u8rb.capacity(), 1024*1024*4);
-    EXPECT_EQ(u8rb.size(), (1024*1024+1)*2);
+        rb.push_back(a);
+    HOPE_EQ(rb.capacity(), 1024*1024*4);
+    HOPE_EQ(rb.size(), (1024*1024+1)*2);
     for(int i=0; i<1024*1024+1; ++i){
-        EXPECT_EQ(u8rb.pop_front(b, 2), 2);
-        EXPECT_STREQ((char*)b, "90");
+        HOPE_EQ(rb.pop_front(b, 2), 2);
+        HOPE_EQ(memcmp(b,"90",2), 0);
     }
-    EXPECT_EQ(u8rb.capacity(), 1024*1024*4);
-    EXPECT_EQ(u8rb.size(), 0);
-    u8rb.reset();
-    EXPECT_EQ(u8rb.capacity(), 0);
+    HOPE_EQ(rb.capacity(), 1024*1024*4);
+    HOPE_EQ(rb.size(), 0);
+
+    rb.reset();
+    HOPE_EQ(rb.capacity(), 0);
 }
 
 
@@ -308,46 +367,17 @@ TEST(ring_byte_buf, 0008){
 }
 
 
-TEST(ring_byte_buf, 0009){
-    ring_byte_buf rb(1024);
-    EXPECT_EQ(rb.empty(), true);
-    EXPECT_EQ(rb.capacity(), 1024);
-    EXPECT_EQ(rb.size(), 0);
-    rb.shrink();
-    EXPECT_EQ(rb.empty(), true);
-    EXPECT_EQ(rb.capacity(), 0);
-    EXPECT_EQ(rb.size(), 0);
-    rb.shrink();
-
-    char a[8];
-    rb.push_back("12345");
-    rb.shrink();
-    EXPECT_EQ(rb.capacity(), 8);
-    rb.pop_back(a, 2);
-    EXPECT_EQ(memcmp(a,"45",2), 0);
-    EXPECT_EQ(rb.capacity(), 8);
-    rb.shrink();
-    EXPECT_EQ(rb.capacity(), 4);
-    rb.pop_front(a, 1);
-    EXPECT_EQ(memcmp(a,"1",1), 0);
-    rb.shrink();
-    EXPECT_EQ(rb.capacity(), 2);
-    rb.pop_front(a, 1);
-    EXPECT_EQ(memcmp(a,"2",1), 0);
-    rb.shrink();
-    EXPECT_EQ(rb.capacity(), 1);
-    rb.pop_front(a, 1);
-    EXPECT_EQ(memcmp(a,"3",1), 0);
-    rb.shrink();
-    EXPECT_EQ(rb.capacity(), 0);
-}
-
 // copy from gtest_main.cc
 int main(int argc, char **argv) {
     test_empty();
     test_push_back();
     test_push_back_2();
     test_push_back_3();
+    test_pop_front();
+    test_pop_front_2();
+    test_push_front_pop_back();
+    test_stress();
+
     printf("Running main() from %s\n", __FILE__);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
