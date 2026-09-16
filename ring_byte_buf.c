aed8cb2e6c010c57c4163f0985fc14e8b98d2991
xinlin-z <xinlin.zhang@hotmail.com>
2026-09-16 15:46:23 +1200

    rewrite the whole logic

--- ring_byte_buf.c
+++ ring_byte_buf.c
@@ -1,158 +1,225 @@
 #include "ring_byte_buf.h"
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>  // -lm
 
 
-static inline void _read_all2buf(ringbb *rb, void *buf){
-    if(rb->_wp >= rb->_rp)  // _wp==_rp when size==0
-        memcpy(buf, rb->_buf+rb->_rp, rb->size);
-    else{
-        size_t rrlen = rb->capacity - rb->_rp;
-        memcpy(buf, rb->_buf+rb->_rp, rrlen);
-        memcpy(buf+rrlen, rb->_buf, rb->_wp);
+static inline size_t _get_needed_size(size_t len)
+{
+    /* never return 0, avoid malloc(0) */
+    if(len == 0)
+        return RBB_BUF_LEN_UNIT;
+
+    /* just cover len bytes with multiples of unit */
+    return len % RBB_BUF_LEN_UNIT == 0 ? len
+           : ((len/RBB_BUF_LEN_UNIT)+1) * RBB_BUF_LEN_UNIT;
+}
+
+
+static inline void _read_all2buf(ringbb *rb, void *buf)
+{
+    if(rb->size == 0)
+        return;
+
+    if(rb->wp > rb->rp)
+        memcpy(buf, rb->buf + rb->rp, rb->size);
+    else
+    {
+        size_t rlen = rb->capacity - rb->rp;
+        memcpy(buf, rb->buf + rb->rp, rlen);
+        memcpy(buf + rlen, rb->buf, rb->wp);
     }
 }
 
 
-static inline bool _recap(ringbb *rb, const void *mem,
-                          size_t len, bool is_push_back){
-    size_t need_size = pow(2.0, (int)ceil(log2(rb->size+len)));
-    unsigned char *buf = (unsigned char*)malloc(need_size);
-    if(!buf)
-        return false;
-
-    if(!is_push_back)
-        memcpy(buf, mem, len);
-    _read_all2buf(rb, buf+(is_push_back?0:len));
-    free(rb->_buf);
-    if(is_push_back)
-        memcpy(buf+rb->size, mem, len);
-
-    rb->_buf = buf;
-    rb->capacity = need_size;
-    rb->size += len;
-    rb->_wp = rb->size;
-    rb->_rp = 0;
-    return true;
-}
-
-
-bool rbb_init(ringbb *rb, size_t len){
-    size_t need_size = pow(2.0, (int)ceil(log2(len)));
-    need_size = need_size<=64 ? 64 : need_size;  // min 64 at init
-    rb->_buf = (unsigned char*)malloc(need_size);
-    if(!rb->_buf)
-        return false;
-    rb->capacity = need_size;
-    rb->size = rb->_wp = rb->_rp = 0;
-    return true;
-}
-
-
-void rbb_free(ringbb *rb){
-    free(rb->_buf);
-    rb->_buf = NULL;
-    rb->capacity = rb->size = rb->_wp = rb->_rp = 0;
-}
-
-
-bool rbb_shrink(ringbb *rb){
-    size_t need_size = pow(2.0, (int)ceil(log2(rb->size)));
-    if(need_size == rb->capacity)
-        return true;
-    unsigned char *buf = (unsigned char*)malloc(need_size);  // might lt 64
-    if(!buf)
-        return false;
+static inline size_t _pop_all(ringbb *rb, void *buf)
+{
+    size_t rt = rb->size;
     _read_all2buf(rb, buf);
-    free(rb->_buf);
-    rb->_buf = buf;
-    rb->capacity = need_size;
-    rb->_wp = rb->size;
-    rb->_rp = 0;
+    rb->size = rb->rp = rb->wp = 0;
+    return rt;
+}
+
+
+static bool _recap(ringbb *rb,
+                   const void *mem,
+                   size_t len,
+                   bool is_push_back)
+{
+    /* rb->size+len might be wrapped, - is safe! */
+    if(len > RBB_BUF_MAX_SIZE - rb->size)
+        return false;
+    size_t needed_size = _get_needed_size(rb->size+len);
+
+    /* len equals zero when shrink */
+    if(len != 0)
+    {
+        /* geometric memory growth until half max size,
+           then grow with fitted unit */
+        size_t doubled_size = rb->capacity * 2;
+        if((doubled_size > needed_size)
+                && (doubled_size <= RBB_BUF_MAX_SIZE))
+            needed_size = doubled_size;
+    }
+
+    unsigned char *buf = (unsigned char*)malloc(needed_size);
+    if(!buf)
+        return false;
+
+    /* copy all bytes to buf */
+    _read_all2buf(rb, buf + (is_push_back ? 0 : len));
+
+    if(len != 0)
+    {
+        if(is_push_back)
+            memcpy(buf + rb->size, mem, len);
+        else
+            memcpy(buf, mem, len);
+    }
+
+    free(rb->buf);
+    rb->buf = buf;
+    rb->capacity = needed_size;
+    rb->size += len;
+    rb->wp = rb->size;
+    rb->rp = 0;
     return true;
 }
 
 
-bool rbb_push_back(ringbb *rb, const void *mem, size_t len){
-    if(len <= rb->capacity - rb->size){
-        if(rb->_wp + len <= rb->capacity){
-            memcpy(rb->_buf+rb->_wp, mem, len);
-            rb->_wp += len;
-        }else{
-            size_t rwlen = rb->capacity - rb->_wp;
-            memcpy(rb->_buf+rb->_wp, mem, rwlen);
-            memcpy(rb->_buf, mem+rwlen, len-rwlen);
-            rb->_wp = len - rwlen;
+bool rbb_init(ringbb *rb, size_t len)
+{
+    if(len > RBB_BUF_MAX_SIZE)
+        return false;
+
+    size_t needed_size = _get_needed_size(len);
+    rb->buf = (unsigned char*)malloc(needed_size);
+    if(!rb->buf)
+        return false;
+
+    rb->capacity = needed_size;
+    rb->size = rb->wp = rb->rp = 0;
+    return true;
+}
+
+
+void rbb_free(ringbb *rb)
+{
+    free(rb->buf);
+    rb->buf = NULL;
+    rb->capacity = rb->size = rb->wp = rb->rp = 0;
+}
+
+
+bool rbb_shrink(ringbb *rb)
+{
+    size_t needed_size = _get_needed_size(rb->size);
+    if(needed_size == rb->capacity)
+        return true;
+
+    return _recap(rb, NULL, 0, true);
+}
+
+
+bool rbb_push_back(ringbb *rb, const void *mem, size_t len)
+{
+    if(len == 0)
+        return true;
+
+    if(len <= rb->capacity - rb->size)
+    {
+        if(rb->wp + len <= rb->capacity)
+        {
+            memcpy(rb->buf + rb->wp, mem, len);
+            rb->wp += len;
+        } else
+        {
+            size_t rlen = rb->capacity - rb->wp;
+            memcpy(rb->buf + rb->wp, mem, rlen);
+            memcpy(rb->buf, mem+rlen, len-rlen);
+            rb->wp = len - rlen;
         }
+
         rb->size += len;
         return true;
-    }else
+    } else
         return _recap(rb, mem, len, true);
 }
 
 
-bool rbb_push_front(ringbb *rb, const void *mem, size_t len){
-    if(len <= rb->capacity-rb->size){
-        if(rb->_rp >= len){
-            memcpy(rb->_buf+(rb->_rp-len), mem, len);
-            rb->_rp -= len;
-        }else{
-            size_t rwlen = len - rb->_rp;
-            rb->_rp = rb->capacity - rwlen;
-            memcpy(rb->_buf+rb->_rp, mem, rwlen);
-            memcpy(rb->_buf, mem+rwlen, len-rwlen);
+bool rbb_push_front(ringbb *rb, const void *mem, size_t len)
+{
+    if(len == 0)
+        return true;
+
+    if(len <= rb->capacity - rb->size)
+    {
+        if(rb->rp >= len)
+        {
+            memcpy(rb->buf + (rb->rp-len), mem, len);
+            rb->rp -= len;
+        } else
+        {
+            size_t rlen = len - rb->rp;
+            rb->rp = rb->capacity - rlen;
+            memcpy(rb->buf + rb->rp, mem, rlen);
+            memcpy(rb->buf, mem+rlen, len-rlen);
         }
+
         rb->size += len;
         return true;
-    }else
+    } else
         return _recap(rb, mem, len, false);
 }
 
 
-size_t rbb_pop_front(ringbb *rb, void *buf, size_t len){
-    size_t rt = len;
-    if(rb->size >= len){
-        if(rb->capacity-rb->_rp >= len){
-            memcpy(buf, rb->_buf+rb->_rp, len);
-            rb->_rp += len;
-        }else{
-            size_t rrlen = rb->capacity - rb->_rp;
-            memcpy(buf, rb->_buf+rb->_rp, rrlen);
-            memcpy(buf+rrlen, rb->_buf, len-rrlen);
-            rb->_rp = len - rrlen;
+size_t rbb_pop_front(ringbb *rb, void *buf, size_t len)
+{
+    if(len == 0)
+        return 0;
+
+    if(rb->size >= len)
+    {
+        if(rb->capacity - rb->rp >= len)
+        {
+            memcpy(buf, rb->buf + rb->rp, len);
+            rb->rp += len;
+        } else
+        {
+            size_t rlen = rb->capacity - rb->rp;
+            memcpy(buf, rb->buf + rb->rp, rlen);
+            memcpy(buf+rlen, rb->buf, len-rlen);
+            rb->rp = len - rlen;
         }
+
         rb->size -= len;
-    }else{
-        _read_all2buf(rb, buf);
-        rb->_rp = rb->_wp;
-        rt = rb->size;
-        rb->size = 0;
-    }
-    return rt;
+        return len;
+    } else
+        return _pop_all(rb, buf);
 }
 
 
-size_t rbb_pop_back(ringbb *rb, void *buf, size_t len){
-    size_t rt = len;
-    if(rb->size >= len){
-        if(rb->_wp >= len){
-            memcpy(buf, rb->_buf+rb->_wp-len, len);
-            rb->_wp -= len;
-        }else{
-            size_t rrlen = len - rb->_wp;
-            rb->_wp = rb->capacity - rrlen;
-            memcpy(buf, rb->_buf+rb->_wp, rrlen);
-            memcpy(buf+rrlen, rb->_buf, len-rrlen);
-        }
-        rb->size -= len;
-    }else{
-        _read_all2buf(rb, buf);
-        rb->_rp = rb->_wp;
-        rt = rb->size;
-        rb->size = 0;
-    }
-    return rt;
-}
+size_t rbb_pop_back(ringbb *rb, void *buf, size_t len)
+{
+    if(len == 0)
+        return 0;
 
+    if(rb->size >= len)
+    {
+        if(rb->wp >= len)
+        {
+            memcpy(buf, rb->buf + rb->wp-len, len);
+            rb->wp -= len;
+        } else
+        {
+            size_t rlen = len - rb->wp;
+            rb->wp = rb->capacity - rlen;
+            memcpy(buf, rb->buf + rb->wp, rlen);
+            memcpy(buf+rlen, rb->buf, len-rlen);
+        }
+
+        rb->size -= len;
+        return len;
+    } else
+        return _pop_all(rb, buf);
+}
 
--- ring_byte_buf.h
+++ ring_byte_buf.h
@@ -1,42 +1,45 @@
 #ifndef RING_BYTE_BUF_H
-#define RING_BYTE_BUF_H 1
-
+#define RING_BYTE_BUF_H
+#include <stddef.h>
+#include <stdint.h>
 #ifndef __cplusplus
-#include <stdio.h>
 #include <stdbool.h>
-#else
-#include <cstdio>
-#include <cstdbool>
 #endif
 
 
-struct ringbb{
+/* both C&C++ support this syntax,
+   but in C++, typedef is redundant */
+typedef struct ringbb{
     size_t capacity;
     size_t size;
-    size_t _rp;
-    size_t _wp;
-    unsigned char *_buf;
-};
+    size_t rp;
+    size_t wp;
+    unsigned char *buf;
+} ringbb;
 
 
-#ifndef __cplusplus
-typedef struct ringbb ringbb;
+/* it also serve as the minimum buffer length */
+#define RBB_BUF_LEN_UNIT     ((size_t)1 << 12)  // 4K
+
+
+#if SIZE_MAX > 0xFFFFFFFFu
+#define RBB_BUF_MAX_SIZE     ((size_t)1 << 32)  // 4G @ 64bit
+#else
+#define RBB_BUF_MAX_SIZE     ((size_t)1 << 30)  // 1G @ 32bit
 #endif
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-bool rbb_init(ringbb*, size_t);
-void rbb_free(ringbb*);
-bool rbb_shrink(ringbb*);
-bool rbb_push_back(ringbb*, const void*, size_t);
-bool rbb_push_front(ringbb*, const void*, size_t);
-size_t rbb_pop_front(ringbb*, void*, size_t);
+bool   rbb_init(ringbb*, size_t);
+void   rbb_free(ringbb*);
+bool   rbb_push_back(ringbb*, const void*, size_t);
+bool   rbb_push_front(ringbb*, const void*, size_t);
 size_t rbb_pop_back(ringbb*, void*, size_t);
+size_t rbb_pop_front(ringbb*, void*, size_t);
+bool   rbb_shrink(ringbb*);
 #ifdef __cplusplus
 }
 #endif
-
-
 #endif
